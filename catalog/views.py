58b6ccfa0d4ccbddcc47be4a7b35342e3df93ba9
Runar Skagestad <runar.skagestad@visma.com>
2019-11-01 16:54:11 +0100

    Lesson 10 (tests) of tutorial

--- catalog/views.py
+++ catalog/views.py
@@ -1,18 +1,17 @@
-from django.shortcuts import render
-from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
-
 import datetime
 
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse, reverse_lazy
+from django.views import generic
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from catalog.forms import RenewBookForm
-from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
-from catalog.models import Book, Author, BookInstance
+from catalog.models import Author, Book, BookInstance
 
 
 def index(request):
@@ -23,36 +22,40 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
     # Number of visits to this view, as counted in the session variable.
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
-        'num_books': num_books,
-        'num_instances': num_instances,
-        'num_instances_available': num_instances_available,
-        'num_authors': num_authors,
-        'num_visits': num_visits,
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+        "num_visits": num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    return render(request, "index.html", context=context)
 
 
-class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to current user."""
-    model = BookInstance
-    template_name ='catalog/bookinstance_list_borrowed_user.html'
-    paginate_by = 10
-    
-    def get_queryset(self):
-        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
+    model = BookInstance
+    template_name = "catalog/bookinstance_list_borrowed_user.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact="o")
+            .order_by("due_back")
+        )
 
 
 class BookListView(generic.ListView):
@@ -67,14 +70,16 @@ class BookDetailView(generic.DetailView):
     Within the template you can access the list of books with the template variable named object OR book
     (i.e. generically "the_model_name").
     """
+
     model = Book
 
-@permission_required('catalog.can_mark_returned')
+
+@permission_required("catalog.can_mark_returned")
 def renew_book_librarian(request, pk):
     book_instance = get_object_or_404(BookInstance, pk=pk)
 
     # If this is a POST request then process the Form data
-    if request.method == 'POST':
+    if request.method == "POST":
 
         # Create a form instance and populate it with data from the request (binding):
         form = RenewBookForm(request.POST)
@@ -82,7 +87,7 @@ def renew_book_librarian(request, pk):
         # Check if the form is valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.due_back = form.cleaned_data["renewal_date"]
             book_instance.save()
 
             # redirect to a new URL:
@@ -91,29 +96,27 @@ def renew_book_librarian(request, pk):
     # If this is a GET (or any other method) create the default form.
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+        form = RenewBookForm(initial={"renewal_date": proposed_renewal_date})
 
     context = {
-        'form': form,
-        'book_instance': book_instance,
+        "form": form,
+        "book_instance": book_instance,
     }
 
-    return render(request, 'catalog/book_renew_librarian.html', context)
+    return render(request, "catalog/book_renew_librarian.html", context)
 
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-
-from catalog.models import Author
 
 class AuthorCreate(CreateView):
     model = Author
-    fields = '__all__'
-    initial = {'date_of_death': '05/01/2018'}
+    fields = "__all__"
+    initial = {"date_of_death": "05/01/2018"}
+
 
 class AuthorUpdate(UpdateView):
     model = Author
-    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    fields = ["first_name", "last_name", "date_of_birth", "date_of_death"]
+
 
 class AuthorDelete(DeleteView):
     model = Author
-    success_url = reverse_lazy('authors')
+    success_url = reverse_lazy("authors")
